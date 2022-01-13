26f4018271f55a79e3de35cdab742b63325a96aa
Michael Kintscher <michael@kintscher.dev>
2022-01-13 13:01:41 -0700

    Set up workspace functions.

--- VirtualDesktopRestore/VirtualDesktopRestore.ps1
+++ VirtualDesktopRestore/VirtualDesktopRestore.ps1
@@ -2,70 +2,152 @@
 # Script.ps1
 #
 
-# Create a new virtual desktop.
-# Based on: https://superuser.com/questions/995236/how-to-create-new-virtual-desktops-in-a-script-to-launch-multiple-applications-i
-<#
-$KeyShortcut = Add-Type -MemberDefinition @"
-[DllImport("user32.dll")]
-static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
-//WIN + CTRL + D: Create a new desktop
-public static void CreateVirtualDesktopInWin10()
-{
-    //Key down
-    keybd_event((byte)0x5B, 0, 0, UIntPtr.Zero); //Left Windows key 
-    keybd_event((byte)0x11, 0, 0, UIntPtr.Zero); //CTRL
-    keybd_event((byte)0x44, 0, 0, UIntPtr.Zero); //D
-    //Key up
-    keybd_event((byte)0x5B, 0, (uint)0x2, UIntPtr.Zero);
-    keybd_event((byte)0x11, 0, (uint)0x2, UIntPtr.Zero);
-    keybd_event((byte)0x44, 0, (uint)0x2, UIntPtr.Zero);
+# ====================== Config Input Functions =======================
+
+function Get-Config-Input {
+    $ConfigFileName = "settings.json"
+    $ConfigData = $null
+
+    # Check if the file exists.
+    if (Test-Path -Path $ConfigFileName -PathType Leaf) {
+        # File exists, so read it.
+        $ConfigData = Get-Content $ConfigFileName | ConvertFrom-Json
+    }
+    else {
+        # File does not exist, so create it.
+        New-Item -Path $ConfigFileName -ItemType File
+    }
 }
-"@ -Name CreateVirtualDesktop -UsingNamespace System.Threading -PassThru
-   $KeyShortcut::CreateVirtualDesktopInWin10()
-#>
-
-#Get-Desktop 1 | Switch-Desktop
-
-# Create a new virtual desktop.
-# Based on: https://github.com/MScholtes/PSVirtualDesktop#:~:text=Get-DesktopName%20-Desktop%20desktop%20Get%20name%20of%20virtual%20desktop.,and%20not%20with%20Powershell%20Core%207.1%20and%20up%21
-# New-Desktop | Set-DesktopName -Name "The new one"
 
 
-# ======================== Function Definitions =======================
+# ======================== Workspace Functions ========================
 
 # Classwork
-function Setup-Classwork {
-    New-Desktop | Set-DesktopName -Name "Classwork"
+function Setup-Research-Classwork {
+    # Create the new desktop.
+    $NewDesktop = New-Desktop | Set-DesktopName -Name "Classwork" -PassThru
+
+    # Launch Slack. Based on: https://stackoverflow.com/questions/32146706/slack-url-to-open-a-channel-from-browser
+    #    Team: 
+    #    Channel: general
+    $NewSlack = Start-Process "slack://channel?id=C02T2MKE4EN&team=T02SN3DJ8DV" -PassThru
+
+    # Move the new apps to the new desktop.
+    $NewSlack[0].MainWindowHandle | Move-Window ($NewDesktop) | Switch-Desktop
+    $NewEdge[0].MainWindowHandle | Move-Window ($NewDesktop) | Switch-Desktop
 }
 
 # Research - XR Ed Survey
 function Setup-Research-XR-Ed-Survey {
-    New-Desktop | Set-DesktopName -Name "Research - XR Survey"
+    # Create the new desktop.
+    $NewDesktop = New-Desktop | Set-DesktopName -Name "Research - XR Survey" -PassThru
+
+    # Launch Edge to display the following websites:
+    #    - Google Chat
+    #    - Google Drive, Team Drive, XR Ed Survey Paper
+    $NewEdge = Create-Browser-Window -URLsToOpen https://mail.google.com/chat/u/1/#chat/dm/ihPWGgAAAAE, https://drive.google.com/drive/u/1/folders/0AOPuz4T3MGdNUk9PVA
+
+    # Move the new apps to the new desktop.
+    $NewEdge[0].MainWindowHandle | Move-Window ($NewDesktop) | Switch-Desktop
 }
 
 # Research - AR Paper
 function Setup-Research-AR-Paper {
-    New-Desktop | Set-DesktopName -Name "Research - AR Paper"
+    # Create the new desktop.
+    $NewDesktop = New-Desktop | Set-DesktopName -Name "Research - AR Paper" -PassThru
+
+    # Launch Edge to display the following websites:
+    #    - Google Chat
+    #    - Google Drive, Team Drive, Thesis - Michael Kintscher
+    $NewEdge = Create-Browser-Window -URLsToOpen https://mail.google.com/chat/u/1/#chat/dm/ihPWGgAAAAE, https://drive.google.com/drive/u/1/folders/0AJbDQ0JQLWIiUk9PVA
+
+    # Move the new apps to the new desktop.
+    $NewEdge[0].MainWindowHandle | Move-Window ($NewDesktop) | Switch-Desktop
 }
 
 # Coding
 function Setup-Coding {
-    New-Desktop | Set-DesktopName -Name "Coding"
+    # Create the new desktop.
+    $NewDesktop = New-Desktop | Set-DesktopName -Name "Coding" -PassThru
+
+    # Launch Edge to display the following websites:
+    #    - GitHub
+    $NewEdge = Create-Browser-Window -URLsToOpen https://github.com/MichaelKintscher
+
+    # Move the new apps to the new desktop.
+    $NewEdge[0].MainWindowHandle | Move-Window ($NewDesktop) | Switch-Desktop
 }
 
 # GSA
-function Setup-GSA {
-    New-Desktop | Set-DesktopName -Name "GSA"
+function Setup-Work-GSA {
+    # Create the new desktop.
+    $NewDesktop = New-Desktop | Set-DesktopName -Name "GSA" -PassThru
+
+    # Launch Edge to display the following websites:
+    #    - Upcoming Zoom Meetings
+    #    - Canvas
+    $NewEdge = Create-Browser-Window -URLsToOpen https://asu.zoom.us/meeting#/upcoming, https://canvas.asu.edu/courses/106062
+
+    # Move the new apps to the new desktop.
+    $NewEdge[0].MainWindowHandle | Move-Window ($NewDesktop) | Switch-Desktop
+}
+
+# GRADient
+function Setup-GRADient {
+    # Create the new desktop.
+    $NewDesktop = New-Desktop | Set-DesktopName -Name "GRADient" -PassThru
+
+    # Launch Slack. Based on: https://stackoverflow.com/questions/32146706/slack-url-to-open-a-channel-from-browser
+    #    Team: GRADient
+    #    Channel: general
+    $NewSlack = Start-Process "slack://channel?id=CCKSDQ57G&team=TCJLVA6TF" -PassThru
+
+    # Launch Edge to display the following websites:
+    #    - Google Drive, Team Drive, GRADient
+    $NewEdge = Create-Browser-Window -URLsToOpen https://drive.google.com/drive/u/1/folders/0AIalcm3xUMfBUk9PVA
+
+    # Move the new apps to the new desktop.
+    $NewSlack[0].MainWindowHandle | Move-Window ($NewDesktop) | Switch-Desktop
+    $NewEdge[0].MainWindowHandle | Move-Window ($NewDesktop) | Switch-Desktop
 }
 
 # Video Production
 function Setup-Video-Production {
-    New-Desktop | Set-DesktopName -Name "Video Production"
+    # Create the new desktop.
+    $NewDesktop = New-Desktop | Set-DesktopName -Name "Video Production" -PassThru | Switch-Desktop
 }
 
-# OA
-function Setup-OA {
-    New-Desktop | Set-DesktopName -Name "OA"
+# NOAC OA
+function Setup-Work-OA-NOAC {
+    # Create the new desktop.
+    $NewDesktop = New-Desktop | Set-DesktopName -Name "OA" -PassThru
+
+    # Launch Slack. Based on: https://stackoverflow.com/questions/32146706/slack-url-to-open-a-channel-from-browser
+    #    Team: 
+    #    Channel: general
+    $NewSlack = Start-Process "slack://channel?id=C061MCJLU&team=T061MGC1W" -PassThru
+
+    # Move the new apps to the new desktop.
+    $NewSlack[0].MainWindowHandle | Move-Window ($NewDesktop) | Switch-Desktop
+}
+
+function Setup-Cross-Workspace-Apps {
+
+    #[Diagnostics.Process[]]$Apps
+
+    # Launch Signal.
+    $NewSignal = Start-Process "[Signal install location]" -PassThru
+    
+    # Launch Calendar.
+    $NewCalendar = Start-Process outlookcal: -PassThru
+
+    # Launch Email.
+    $NewEmail = Start-Process "ms-unistore-email://" -PassThru
+
+    # Pin each app.
+    #foreach ($newApp in $Apps) {
+        #Pin-Application ($newApp[0].MainWindowHandle)
+    #}
 }
 
 function Create-Browser-Window {
@@ -77,7 +159,7 @@ function Create-Browser-Window {
     # Based on: https://stackoverflow.com/questions/40493141/how-to-utilize-powershell-to-open-an-application-with-a-command
     #     Starts a new window.
     #     -PassThru is needed to enable output to be stored in the variable. See: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/start-process?view=powershell-7.2
-    $NewEdge = Start-Process -FilePath "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -PassThru
+    $NewEdge = Start-Process -FilePath "[Edge install location]" -PassThru
 
     # Start a new tab for each URL to open.
     foreach ($URLParam in $URLsToOpen) {
@@ -87,6 +169,27 @@ function Create-Browser-Window {
 
     # Return the handle to the new browser window.
     return $NewEdge
+}
+
+
+# ========================== Test Functions ===========================
+
+function Test-Browser-Launch {
+    # -PassThru is needed to enable output to be stored in the variable. See: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/start-process?view=powershell-7.2
+    $NewDesktop = New-Desktop | Set-DesktopName -Name "W" -PassThru
+    $NewEdge = Create-Browser-Window -URLsToOpen http://google.com, http://aa.com
+    #Start-Sleep -Milliseconds 2000
+    $NewEdge.MainWindowHandle | Move-Window ($NewDesktop) | Switch-Desktop
+    #Move-Window -Desktop ($NewDesktop) -Hwnd ($NewEdge.MainWindowHandle) | Switch-Desktop
+    Write-Output $NewDesktop
+    Write-Output $NewEdge
+    Pin-Window ($NewEdge.MainWindowHandle)
+}
+
+function Test-App-Launch {
+    # Launch Email.
+    $NewEmail = Start-Process -FilePath "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -PassThru
+    Write-Output $NewEmail[0].MainWindowHandle
 }
 
 
@@ -105,13 +208,10 @@ elseif ($Today -eq "Tuesday") {
     New-Desktop | Set-DesktopName -Name "T"
 }
 elseif ($Today -eq "Wednesday") {
-    # -PassThru is needed to enable output to be stored in the variable. See: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/start-process?view=powershell-7.2
-    $NewDesktop = New-Desktop | Set-DesktopName -Name "W" -PassThru
-    $NewEdge = Create-Browser-Window -URLsToOpen http://google.com, http://aa.com
-    $NewEdge[0].MainWindowHandle | Move-Window ($NewDesktop) | Switch-Desktop
+    New-Desktop | Set-DesktopName -Name "W"
 }
 elseif ($Today -eq "Thursday") {
-    New-Desktop | Set-DesktopName -Name "Th"
+    
 }
 elseif ($Today -eq "Friday") {
     New-Desktop | Set-DesktopName -Name "F"
@@ -122,3 +222,9 @@ elseif ($Today -eq "Saturday") {
 else {
     New-Desktop | Set-DesktopName -Name "U"
 }
+
+Get-Config-Input
+
+#Test-App-Launch
+#Test-Browser-Launch
+#Setup-Cross-Workspace-Apps
