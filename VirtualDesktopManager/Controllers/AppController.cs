f0aa2263c7af9f639cc89ae7c51ea04e94a77b89
Michael Kintscher <michael@kintscher.dev>
2022-01-16 01:14:14 -0700

    Added app project for GUI to manage script's data.

---
+++ VirtualDesktopManager/Controllers/AppController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualDesktopManager.Controllers
+{
+    /// <summary>
+    /// The main controller for the app - handles navigation between app-level processes.
+    /// </summary>
+    internal class AppController : SingletonController<AppController>
+    {
+        #region Properties
+        /// <summary>
+        /// The root page for app navigation.
+        /// </summary>
+        public MainWindow RootPage { get; private set; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Intended to be called upon app launch. Initializes app navigation.
+        /// </summary>
+        /// <param name="rootPage">The root page for this initialization of the app.</param>
+        public void StartApp(MainWindow rootPage)
+        {
+            // Subscribe to the root page's events.
+
+            // Set the given page as the root page.
+            this.RootPage = rootPage;
+        }
+        #endregion
+    }
+}
---
+++ VirtualDesktopManager/Controllers/SingletonController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualDesktopManager.Controllers
+{
+    /// <summary>
+    /// Provides a base class for controllers that implements the
+    /// singleton design pattern using an Instance property.
+    /// </summary>
+    /// <typeparam name="T">The type of the controller, which must implement a public parameterless constructor.</typeparam>
+    internal abstract class SingletonController<T> where T : new()
+    {
+        /// <summary>
+        /// The static reference to the only (singleton) instance of the class.
+        /// </summary>
+        private static readonly Lazy<T> _mySingleton = new Lazy<T>(() => new T());
+
+        /// <summary>
+        /// Returns the single instance of the class.
+        /// </summary>
+        public static T Instance
+        {
+            get => SingletonController<T>._mySingleton.Value;
+        }
+    }
+}
