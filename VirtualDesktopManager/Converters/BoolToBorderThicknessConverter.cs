ef8d95ff6b3bbd336ffe23561e559a5da385658c
Michael Kintscher <michael@kintscher.dev>
2022-01-23 20:46:26 -0700

    Days of the week now displayed for workspaces in the details popup.

---
+++ VirtualDesktopManager/Converters/BoolToBorderThicknessConverter.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualDesktopManager.Converters
+{
+    /// <summary>
+    /// Converts a bool to a border thickness. The convert back behavior treats any border thickness aside from the "true" thickness as false.
+    /// </summary>
+    internal class BoolToBorderThicknessConverter : IValueConverter
+    {
+        /// <summary>
+        /// The thickness to return if the input is true.
+        /// </summary>
+        public Thickness TrueThickness { get; set; }
+
+        /// <summary>
+        /// The thickness to return if the input is false.
+        /// </summary>
+        public Thickness FalseThickness { get; set; }
+
+        public BoolToBorderThicknessConverter()
+        {
+
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool boolValue = (bool)value;
+
+            return boolValue ? this.TrueThickness : this.FalseThickness;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            bool convertedValue;
+
+            convertedValue = (Thickness)value == this.TrueThickness;
+
+            return convertedValue;
+        }
+    }
+}
---
+++ VirtualDesktopManager/Converters/BoolToSolidColorConverter.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace VirtualDesktopManager.Converters
+{
+    /// <summary>
+    /// Converts a bool to one of the given colors (as a brush). The convert back behavior treats any color aside from the "true" color as false.
+    /// </summary>
+    public class BoolToSolidColorBrushConverter : IValueConverter
+    {
+        /// <summary>
+        /// The color to return if the input is true.
+        /// </summary>
+        public Color TrueColor { get; set; }
+
+        /// <summary>
+        /// The color to return if the input is false.
+        /// </summary>
+        public Color FalseColor { get; set; }
+
+        public BoolToSolidColorBrushConverter()
+        {
+
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool boolValue = (bool)value;
+
+            return boolValue ? new SolidColorBrush(this.TrueColor) : new SolidColorBrush(this.FalseColor);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            bool convertedValue;
+
+            convertedValue = (Color)value == this.TrueColor;
+
+            return convertedValue;
+        }
+    }
+}
