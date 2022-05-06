192281d862ffbcfb12e24ce972fa99afbc4b8219
Michael Kintscher <michael@kintscher.dev>
2022-05-06 13:14:39 -0700

    Displays cmd args

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import sys
+import tkinter as tk
 
-print("Hello World")
+root= tk.Tk()
 
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
+canvas1 = tk.Canvas(root, width = 300, height = 300)
+canvas1.pack()
+
+def hello ():  
+    btnText = 'Argument List:' + str(sys.argv)
+    label1 = tk.Label(root, text= btnText, fg='green', font=('helvetica', 12, 'bold'))
+    canvas1.create_window(150, 200, window=label1)
+    
+button1 = tk.Button(text='Click Me',command=hello, bg='brown',fg='white')
+canvas1.create_window(150, 150, window=button1)
+
+root.mainloop()
