d082538e85bd31a2f78e44be467847fdd439a37a
Michael Kintscher <michael@kintscher.dev>
2022-05-06 17:59:08 -0700

    Moved arg echo to test.py. Added dependency to pyvda. Now counts virutal desktops open.

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
+# Creates a window with a button in it. Clicking the button displays the command line arguments passed to the script. The first argument will always be the filepath of the script.
 import sys
 import tkinter as tk
-from pyvda import AppView, get_apps_by_z_order, VirtualDesktop, get_virtual_desktops
-
-# ====================== Virtual Desktop Functions ==========================
-
-def getNumDesktops():
-    return len(get_virtual_desktops())
-
-# ===========================================================================
 
 root= tk.Tk()
 
@@ -15,8 +8,7 @@ canvas1 = tk.Canvas(root, width = 300, height = 300)
 canvas1.pack()
 
 def hello ():  
-    btnText = 'You have ' + str(getNumDesktops()) + ' virtual desktops open!'
-    print(btnText)
+    btnText = 'Argument List:' + str(sys.argv)
     label1 = tk.Label(root, text= btnText, fg='green', font=('helvetica', 12, 'bold'))
     canvas1.create_window(150, 200, window=label1)
     
